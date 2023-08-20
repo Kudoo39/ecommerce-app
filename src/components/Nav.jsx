4f4f43ff037a7b47da83f0cc3fc4a74dabcddf58
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-20 14:56:56 +0300

    adding nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul className="nav-list">
+        <a href="">Home</a>
+        <a href="">Menu</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
