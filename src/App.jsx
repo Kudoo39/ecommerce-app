11b4c8b7f12c513be923c3efa7aef82fd7a33b19
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-20 20:05:43 +0300

    adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Nav />
       <Header />
       <Home />
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="" className="footer">
+        SAIGON SANDWICH BAKERY
+      </a>
+      <ul className="links">
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+      </ul>
+      <div className="footer_socials">
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
