a194da46209a144e2fce785aee89b1b9502ffbd0
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-20 17:17:04 +0300

    customizing UI

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import img1 from "../assets/vegan.jpg";
+import img2 from "../assets/pho.jpg";
 import "./Home.css";
 
 const Home = () => (
@@ -14,11 +15,12 @@ const Home = () => (
         </p>
       </article>
       <article className="banhmi_item">
-        <img src={img1} alt="photo about banhmi" />
-        <h3>Banh Mi</h3>
+        <img src={img2} alt="photo about pho" />
+        <h3>Pho</h3>
         <p>
-          Seasoned seitan, cucumber, radish, carrot, soy, coriander, onion,
-          house sauce, chili, mayonnaise
+          Pho is a popular food in Vietnam, where it is served in households,
+          street stalls and restaurants across the country. It is considered the
+          national dish of Vietnam.
         </p>
       </article>
       <article className="banhmi_item">
