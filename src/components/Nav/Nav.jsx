232908661915cefc8ee1bfd3ab80e1c488944b71
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-09-01 01:19:34 +0300

    adding toggle button for small devices

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.css";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <nav>
-      <ul className="nav-list">
+      <ul className={`nav-list ${isNavOpen ? "open" : ""}`}>
         <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
         <Link to="/hour">Operating Hours</Link>
       </ul>
+
+      <button
+        className={`nav-toggle ${isNavOpen ? "open" : ""}`}
+        onClick={toggleNav}
+      >
+        <span className="line"></span>
+        <span className="line"></span>
+        <span className="line"></span>
+      </button>
     </nav>
   );
 };
