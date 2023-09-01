50c5d20bb614b7fe74b5b67cbce6f689ac10400a
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-09-01 22:54:15 +0300

    update translate for Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,42 +3,33 @@ import img1 from "../../assets/vegan.jpg";
 import img2 from "../../assets/pho.jpg";
 import img3 from "../../assets/owner.jpg";
 import "./Home.css";
+import { useTranslation } from "react-i18next";
 
-const Home = () => (
-  <div id="home">
-    <div className="home_container">
-      <article className="banhmi_item">
-        <img src={img1} alt="photo about banhmi" />
-        <h3>Banh Mi</h3>
-        <p>
-          Seasoned seitan, cucumber, radish, carrot, soy, coriander, onion,
-          house sauce, chili, mayonnaise.
-        </p>
-      </article>
-      <article className="banhmi_item">
-        <img src={img2} alt="photo about pho" />
-        <h3>Pho</h3>
-        <p>
-          Pho is a popular food in Vietnam, where it is served in households,
-          street stalls and restaurants across the country. It is considered the
-          national dish of Vietnam.
-        </p>
-      </article>
-      <article className="banhmi_item">
-        <img id="owner" src={img3} alt="photo about the owner" />
-        <h3>Kim Huong Nguyen</h3>
-        <p>
-          Kim Huong Nguyen opened the Turku street food restaurant Saigon
-          Sandwich Bakery, which has received positive feedback. The goal is to
-          provide genuine Vietnamese flavors.
-        </p>
-        <p>
-          "I started developing recipes and wanted to offer Vietnamese flavors
-          for Finns to taste."
-        </p>
-      </article>
+const Home = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div id="home">
+      <div className="home_container">
+        <article className="banhmi_item">
+          <img src={img1} alt="photo about banhmi" />
+          <h3>Banh Mi</h3>
+          <p>{t("home.banhmiDescription")}</p>
+        </article>
+        <article className="banhmi_item">
+          <img src={img2} alt="photo about pho" />
+          <h3>Pho</h3>
+          <p>{t("home.phoDescription")}</p>
+        </article>
+        <article className="banhmi_item">
+          <img id="owner" src={img3} alt="photo about the owner" />
+          <h3>Kim Huong Nguyen</h3>
+          <p>{t("home.ownerDescription1")}</p>
+          <p>{t("home.ownerDescription2")}</p>
+        </article>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
