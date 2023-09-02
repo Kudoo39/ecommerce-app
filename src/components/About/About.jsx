0f0e4576514b475dd7eb4f4f0d4affef2335965e
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-09-02 23:18:26 +0300

    updating About languages

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,57 +4,40 @@ import img from "../../assets/store.jpg";
 import ceo from "../../assets/ceo.jpg";
 import maker from "../../assets/owner.jpg";
 import staff from "../../assets/staff.jpg";
-
-const corporateLeadership = [
-  {
-    name: "Kim Huong Nguyen",
-    position: "Founder",
-    description:
-      "Kim Huong Nguyen wields her culinary expertise to orchestrate the gastronomic experiences that define the store. Her responsibilities span the entire spectrum of food management, from meticulously selecting the finest ingredients to crafting delectable dishes that delight the palate. With a keen eye for flavor and presentation, Kim Huong ensures that every meal served is a masterpiece in taste and aesthetics.",
-    image: maker,
-  },
-  {
-    name: "Thuy Tien Nguyen",
-    position: "Staff",
-    description:
-      "As a valued member of our staff, Thuy Tien Nguyen embodies the essence of customer service excellence. With a warm and welcoming demeanor, Thuy Tien is dedicated to providing exceptional sales assistance to our esteemed customers. By offering personalized recommendations and addressing inquiries, she contributes to creating a shopping experience that is both enjoyable and satisfying for all patrons.",
-    image: staff,
-  },
-];
+import { useTranslation } from "react-i18next";
 
 const About = () => {
+  const { t } = useTranslation();
+
+  const corporateLeadership = [
+    {
+      name: "Kim Huong Nguyen",
+      position: t("about.founder.position"),
+      description: t("about.founder.description"),
+      image: maker,
+    },
+    {
+      name: "Thuy Tien Nguyen",
+      position: t("about.staff.position"),
+      description: t("about.staff.description"),
+      image: staff,
+    },
+  ];
+
   return (
     <div className="about-container">
-      <h1 className="about-heading">About Us</h1>
+      <h1 className="about-heading">{t("about.title")}</h1>
       <div className="about-hstack">
         <img src={img} className="about-photo" />
         <div className="about-section">
-          <p>
-            Started in 2018, Kim Huong Nguyen opened the street food restaurant
-            Saigon Sandwich Bakery in Turku's Kauppahalli, and the street food
-            stand has been well received. The goal is to offer authentic
-            Vietnamese flavors. At first, Kim Huong Nguyen had time to establish
-            a street food stand on Puistokatu.
-          </p>
-          <p>
-            The French brought white bread to Vietnam. The Vietnamese developed
-            fillings between the bread. Freshly baked bread should be crispy and
-            crumbly on the outside, but very soft on the inside, Kim Huong
-            Nguyen describes. Kim Huong Nguyen continues that it took him three
-            years to develop the recipe for the bread served in Turku, so that
-            the end result was good and the bread is as crispy on the surface
-            and soft on the inside as what is served on the streets of Saigon.
-          </p>
-          <p>
-            In Turku's Kauppahalli, Saigon Sandwich sells about a hundred loaves
-            of bread a day. There are many filling options: you can choose
-            chicken, pork, beef, seitan or soy.
-          </p>
+          <p>{t("about.description1")}</p>
+          <p>{t("about.description2")}</p>
+          <p>{t("about.description3")}</p>
         </div>
       </div>
 
       <div className="leadership-section">
-        <h3>Corporate Leadership</h3>
+        <h3>{t("about.leadershipTitle")}</h3>
         <div className="leadership-grid">
           {corporateLeadership.map((leader, index) => (
             <div key={index} className="leader-card">
