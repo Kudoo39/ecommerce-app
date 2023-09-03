906ffcdf6fed4654aab3db788eb88a98516ef831
Khang Nguyen <83585039+Kudoo39@users.noreply.github.com>
2023-09-03 14:55:05 +0300

    modify base for both deployment methods

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  //to deploy on netlify
+  base: "/", 
+
+  //to deploy on github page
+  //base: "/restaurant-app"
   plugins: [react()],
 });
