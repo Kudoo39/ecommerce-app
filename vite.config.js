371f9ef99f6b0bd6fbbea440d42ff5b1345889f5
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-10-09 19:07:07 +0300

    fixing bug

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   //to deploy on netlify
-  base: "/", 
+  base: "/restaurant-app",
 
   //to deploy on github page
   //base: "/restaurant-app"
